4e28ae30d9925ffe8536811e573177354a3fbaf6
gerardorootsss <gerardocavieres@hotmail.com>
2025-02-08 16:07:44 -0300

    pausa

--- app.js
+++ app.js
@@ -4,25 +4,22 @@ let amigo = [];
 
 // Implementa una función para agregar amigos
 function agregarAmigo(){
-    let listaAmigos = document.getElementById("amigo").value; //Capturar el valor del campo de entrada
-
-        if (amigo === ""){ //Validar la entrada
+    const inputAmigo = document.getElementById("amigo").value; //Capturar el valor del campo de entrada
+        if (inputAmigo === ""){ //Validar la entrada
             alert("Por favor, inserte un nombre.");
             return;
         }
         // valida si el nombre ingresado ya fuera en el array 
-        if (amigo.incluides(listaAmigos)){
-            alert(`El nombre ${listaAmigos} ya fue ingresado.`)
+        if (amigo.includes(inputAmigo)){
+            alert(`El nombre ${inputAmigo} ya fue ingresado.`);
+            return;
         }
-
         // agrega el nombre ingreso a la lista 
-        amigo.push(listaAmigos);
-
+        amigo.push(inputAmigo);
         // limpiar campo de entrada
-        listaAmigos.value = "";
-
+        inputAmigo.value = "";
         // actualizar lista en HTML 
-        actualizarArray();
+        actualizarLista();
        
 }
 
@@ -31,14 +28,14 @@ function agregarAmigo(){
 // Obtener el elemento de la lista
 
 function actualizarLista(){
-    let listaAmigos = document.getElementById("listaAmigos");
-
+    const listaAmigos = document.getElementById("listaAmigos");
+    
 // limpiar contenido de la lista 
     listaAmigos.innerHTML = "";
 
 // Iterar sobre el arreglo
-    for (let i = 0; i<listaAmigos.lengt; i++){
-        let li = docoument.createElement("li");
+    for (let i = 0; i<amigo.length; i++){
+        const li = document.createElement("li");
         li.textContent = amigo[i];
         listaAmigos.appendChild(li); 
     }
@@ -49,20 +46,20 @@ function actualizarLista(){
 function sortearAmigo(){
     //validar que haya amigos disponibles
     if (amigo.length === 0){ // comprueba si array "amigos" esta vcacio
-        alert ("No hay amigos disponibles para sortear. Agrega al menos 2.");
+        alert("No hay amigos disponibles para sortear. Agrega al menos 2.");
         return;
     }
 
     // genera un indice aleatorio
-    let indiceAleatorio = Math.floor(Math.random() * amigo.length); 
+    const indiceAleatorio = Math.floor(Math.random() * amigo.length); 
 
     // genera un numero aleatorio entre 0 y la longitud de la lista
 
-    let amigoSorteado = amigo(indiceAleatorio); // usa el indice aleatorio para obtener un nombre del array 
+    const amigoSorteado = amigo(indiceAleatorio); // usa el indice aleatorio para obtener un nombre del array 
 
     // mostrar el resultado en el HTML 
     
-    let resultado = document.getElementById("resultado")
-    resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong>`;
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `Amigo sorteado: ${amigoSorteado}`;
 
 }
